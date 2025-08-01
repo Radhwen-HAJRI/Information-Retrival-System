3e9b6367b12d72c54c2908b5e64d22155bdcb5a3
Radhwen Hajri <radhwenhajri@hotmail.com>
2025-08-01 14:40:34 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Gererative AI Project',
+    version='0.0.0',
+    author='Hajri',
+    author_email="hajriradhwen@hotmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    
+
+)
